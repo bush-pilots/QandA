bedd8296300bdc8720833163eb940195d6dafcd1
Glenmore Vinoya <glenmore@pinoyak.com>
2021-03-16 18:08:25 -0800

    final config for minimal express/node server completed base config added eslint airbnb style linting, cors, json and urlencoded parsing

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import 'dotenv/config';
+import cors from 'cors';
+import express from 'express';
 
-console.log('Hello Node.js project');
-console.log(process.env.MY_SECRET);
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => res.send('Welcome to my world!'));
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}!`);
+});
