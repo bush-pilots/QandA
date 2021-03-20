9991c0099f1dc721eb3ad118ed0de6969263c629
Glenmore Vinoya <glenmore@pinoyak.com>
2021-03-20 15:59:53 -0800

    added some code fixing due to data format mistake

---
+++ schema.sql
@@ -0,0 +1,79 @@
+DROP DATABASE qaapidb;
+CREATE DATABASE qaapidb;
+
+\c qaapidb;
+
+-- DROP ALL TABLES --
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS questions CASCADE;
+DROP TABLE IF EXISTS answers CASCADE;
+DROP TABLE IF EXISTS photos CASCADE;
+
+CREATE TABLE IF NOT EXISTS questions (
+  id SERIAL PRIMARY KEY NOT NULL,
+  product_id INT NOT NULL,
+  body TEXT NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  username VARCHAR(50) DEFAULT 'guest',
+  email VARCHAR(50) DEFAULT 'nobody@guest.com',
+  reported BOOLEAN DEFAULT FALSE,
+  helpfulness INT DEFAULT 0
+);
+
+CREATE TABLE IF NOT EXISTS answers (
+  id SERIAL PRIMARY KEY NOT NULL,
+  question_id INT NOT NULL REFERENCES questions(id),
+  body TEXT NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW(),
+  username VARCHAR(50) DEFAULT 'guest',
+  email VARCHAR(50) DEFAULT 'nobody@guest.com',
+  reported BOOLEAN DEFAULT FALSE,
+  helpfulness INT DEFAULT 0
+);
+
+CREATE TABLE IF NOT EXISTS photos (
+  id SERIAL PRIMARY KEY NOT NULL,
+  answer_id INT NOT NULL REFERENCES answers(id),
+  url TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  username VARCHAR(100) UNIQUE DEFAULT 'guest',
+  email VARCHAR(50) UNIQUE DEFAULT 'guest@anon.com'
+);
+
+COPY questions FROM '/home/kuyavinny/coding/questions.csv' CSV HEADER;
+COPY answers FROM '/home/kuyavinny/coding/answers.csv' CSV HEADER;
+COPY photos FROM '/home/kuyavinny/coding/answers_photos.csv' CSV HEADER;
+
+SELECT pg_catalog.setval(pg_get_serial_sequence('questions', 'id'), MAX(id)) FROM questions;
+SELECT pg_catalog.setval(pg_get_serial_sequence('answers', 'id'), MAX(id)) FROM answers;
+SELECT pg_catalog.setval(pg_get_serial_sequence('photos', 'id'), MAX(id)) FROM photos;
+
+-- TIMING REPORTS BEFORE INDEXING AFTER A FRESH IMPORT
+\timing
+
+SELECT * FROM questions LIMIT 5;
+SELECT * FROM answers LIMIT 5;
+SELECT * FROM photos LIMIT 5;
+
+\timing
+
+-- INDEX OPTIMIZATIONS
+
+CREATE INDEX questions_id ON questions (id);
+CREATE INDEX questions_product_id ON questions (id, product_id);
+CREATE INDEX answers_id ON answers (id);
+CREATE INDEX answers_question_id ON answers (id, question_id);
+CREATE INDEX photos_id ON photos (id);
+CREATE INDEX photos_answer_id ON photos (id, answer_id);
+
+-- TIMING REPORTS AFTER INDEXING AFTER A FRESH IMPORT
+\timing
+
+SELECT * FROM questions LIMIT 5;
+SELECT * FROM answers LIMIT 5;
+SELECT * FROM photos LIMIT 5;
+
+\timing
---
+++ src/models/Question.js
@@ -0,0 +1,70 @@
+import query from '../db/index';
+import Answers from './Answer';
+
+exports.queryGetQuestions = async (productId, limit = 5, page = 1) => {
+  try {
+    const text = `
+    SELECT
+    id AS question_id,
+    body AS question_body,
+    created_at AS question_date,
+    username AS asker_name,
+    helpfulness AS question_helpfulness,
+    reported
+    FROM questions
+    WHERE product_id = $1
+    LIMIT $2
+    `;
+    const { rows } = await query(text, [productId, limit]);
+    const questions = await Promise.all(rows.map(async (question) => ({
+      ...question,
+      answers: await Answers.queryGetAnswers(question.question_id, limit, page, true),
+    })));
+    return {
+      status: true,
+      data: { product_id: productId, results: questions },
+    };
+  } catch (error) {
+    return { status: false, data: 'Product does not contain any questions.' };
+  }
+};
+
+exports.queryAddQuestion = async ({
+  body, username, email, productId,
+}) => {
+  try {
+    const results = await query({
+      text: 'INSERT INTO questions (product_id, body, username, email) VALUES ($1, $2, $3, $4) RETURNING id',
+      values: [productId, body, username, email],
+    });
+    return { status: true, data: results };
+  } catch (error) {
+    return { status: false, data: error };
+  }
+};
+
+exports.queryMarkQuestionHelpful = async (questionId) => {
+  try {
+    const result = await query(
+      `UPDATE questions
+       SET helpfulness = helpfulness + 1
+       WHERE id = $1`, [questionId],
+    );
+    return { status: true, data: result };
+  } catch (error) {
+    return { status: false, data: error };
+  }
+};
+
+exports.queryReportQuestion = async (questionId) => {
+  try {
+    const result = await query(
+      `UPDATE questions
+       SET reported = $1
+       WHERE id = $2`, [true, questionId],
+    );
+    return { status: true, data: result };
+  } catch (error) {
+    return { status: false, data: error };
+  }
+};
